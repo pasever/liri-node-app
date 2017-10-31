dd82debaff45f0f7564389fb9040b63e99458da2
Pasever <pas1034ever@gmail.com>
2017-10-30 21:28:58 -0400

    a.0.1

---
+++ keys.js
@@ -0,0 +1,13 @@
+
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '4iKlbq6G6nr3zZTF2tIVZLAwS',
+  consumer_secret: 'lS29Yu5wRqIeFYc5EWAobq9tAUWzofqLIqTP2MPwglA3cQ9nMu',
+  access_token_key: '925167641995698178-h7zeDQSiuGg6HlySo85Uq9lJNDdpcKH',
+  access_token_secret: 'riR1AXgUusrYGGLDnyAWB4oIXPhQCDhMF7GBJsf0NSLme'
+}
+
+module.exports = twitterKeys;
+
